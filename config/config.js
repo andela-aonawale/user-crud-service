5c54c209dbf79afb1b83ee9a3844233ff08a2109
ahmed onawale <ahmed.onawale@andela.co>
2015-04-10 18:48:12 +0100

    set up configuration for users and test databases

---
+++ config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+	db: {
+
+		user: {
+			connection: {
+				host: 'localhost',
+				port: '5432',
+				user: 'ahmed',
+				password: 'pr0t0c0l',
+				database: 'userdb',
+				charset: 'utf8'
+			},
+			name: 'users'
+		},
+
+		test: {
+			connection: {
+				host: 'localhost',
+				port: '5432',
+				user: 'ahmed',
+				password: 'pr0t0c0l',
+				database: 'userdb',
+				charset: 'utf8'
+			},
+			name: 'test'
+		}
+
+	}
+	
+}
+
