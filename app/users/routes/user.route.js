cb37a0856370cd578434ab243d71adbd7ec7c63a
ahmed onawale <ahmed.onawale@andela.co>
2015-04-13 22:15:19 +0100

    added signin and signout routes, renamed the put route to /users/edit and delete to /users/delete

--- app/users/routes/user.route.js
+++ app/users/routes/user.route.js
@@ -3,7 +3,9 @@ var users = require('../controllers/user.controller');
 module.exports = function(router){
 	router.route('/users').get(users.getAllUser);
 	router.route('/users/:username').get(users.getOneUser);
-	router.route('/users').post(users.createUser);
-	router.route('/users/:username').put(users.updateUser);
-	router.route('/users/:username').delete(users.deleteUser);
+	router.route('/users/signup').post(users.createUser);
+	router.route('/users/edit').put(users.updateUser);
+	router.route('/users/delete').delete(users.deleteUser);
+	router.route('/users/signin').post(users.signInUser);
+	router.route('/users/signout').post(users.signOutUser);
 };
