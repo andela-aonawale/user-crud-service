d6a0b2980ebd52c1cf7a09f50c21f77c22d99fe9
ahmed onawale <ahmed.onawale@andela.co>
2015-04-13 22:09:22 +0100

    added signin and  signout functions, implemented jsonwebtoken, used crypto-js to hash the user password before saving it into the database

--- app/users/controllers/user.controller.js
+++ app/users/controllers/user.controller.js
@@ -1,4 +1,9 @@
 var User = require('../models/user.model')[1];
+var jwt = require('jsonwebtoken');
+var crypto = require('crypto-js/sha256');
+var secret = require('../../../config/config').db[process.env.NODE_ENV].secret;
+var token;
+
 module.exports = {
 
 	// get all users
@@ -17,8 +22,28 @@ module.exports = {
 
 	// create a new user
 	createUser: function(req, res){
+		token = jwt.sign({username: req.body.username, email: req.body.email}, secret);
+		req.body.token = token;
+		req.body.password = crypto(req.body.password);
 		User.forge(req.body).save().then(function(model){
-			res.json({message: "New User Created"});
+			res.json({message: "User Created", "token": model.attributes.token});
+		});
+	},
+
+	// signIn to user account
+	signInUser: function(req, res){
+		new User({username: req.body.username, password: crypto(req.body.password)})
+		.fetch().then(function(model){
+			res.json({message: "User Logged in", token: model.attributes.token});
+		});
+	},
+
+	// signOut from user account
+	signOutUser: function(req, res){
+		new User({username: req.body.username}).fetch().then(function(model){
+			model.set({"token": ""});
+			model.save();
+			res.json({message: "User Logged Out"});
 		});
 	},
 
