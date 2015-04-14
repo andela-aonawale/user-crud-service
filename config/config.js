5100600adabc2eb4344d3f8b768a5fd06b9e28ff
ahmed onawale <ahmed.onawale@andela.co>
2015-04-14 14:44:59 +0100

    edit the connection options to heroku's

--- config/config.js
+++ config/config.js
@@ -4,15 +4,15 @@ module.exports = {
 
 		user: {
 			connection: {
-				host: 'localhost',
+				host: 'ec2-54-221-249-3.compute-1.amazonaws.com',
 				port: '5432',
-				user: 'ahmed',
-				password: 'pr0t0c0l',
-				database: 'userdb',
+				user: 'uckedtmwbbavrc',
+				password: 'Tyq-83IyZY9qkjYmA43M4AgU5w',
+				database: 'ddnbiadjc5oll8',
 				charset: 'utf8'
 			},
-			name: 'users',
-			secret: 'ammyreal'
+			name: 'user',
+			secret: '@ammyreal'
 		},
 
 		test: {
