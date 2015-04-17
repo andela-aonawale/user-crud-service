726955c58cc28c174556f212f6bbeda5efa8005b
ahmed onawale <ahmed.onawale@andela.co>
2015-04-17 11:41:13 +0100

    asigned function: getOneUser, updateUser, deleteUser and signOutUser to /user/:username route

--- app/users/routes/user.route.js
+++ app/users/routes/user.route.js
@@ -1,11 +1,12 @@
 var users = require('../controllers/user.controller');
 
 module.exports = function(router){
+	router.route('/signup').post(users.createUser);
+	router.route('/signin').post(users.signInUser);
 	router.route('/users').get(users.getAllUser);
-	router.route('/users/:username').get(users.getOneUser);
-	router.route('/users/signup').post(users.createUser);
-	router.route('/users/edit').put(users.updateUser);
-	router.route('/users/delete').delete(users.deleteUser);
-	router.route('/users/signin').post(users.signInUser);
-	router.route('/users/signout').post(users.signOutUser);
+	router.route('/user/:username')
+	.get(users.getOneUser)
+	.put(users.updateUser)
+	.delete(users.deleteUser)
+	.post(users.signOutUser);
 };
